8b90d5b6e33851689802f6cc652cf452c7a60b37
Vladislav <vladislav.khripunov@gmail.com>
2017-09-20 17:50:39 +0700

    Write class Blog in models.oy

--- Django_netghost/app/models.py
+++ Django_netghost/app/models.py
@@ -1,7 +1,31 @@
 """
 Definition of models.
 """
-
+from taggit.managers import TaggableManager
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.core.urlresolvers import reverse
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length  = 100, unique_for_date = "posted", verbose_name = "Заголовок")
+    description = models.TextField(verbose_name = "Краткое содержание")
+    content = models.TextField(verbose_name = "Полное содержание")
+    posted = models.DateTimeField(default = datetime.now(), db_index = True, verbose_name = "Опубликована")
+    is_commentable = models.BooleanField(default = True, verbose_name = "Разрешены комментарии")
+    tags = TaggableManager(blank = True, verbose_name = "Теги")
+    user = models.ForeignKey(User, editable = False)
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs = {"pk": self.pk})
+    class Meta:
+        ordering = ["-posted"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"
 
 # Create your models here.
+from django.contrib.comments.moderation import CommentModerator, moderator
+class BlogModerator(CommentModerator):
+    email_notification = True
+    enable_field = "is_commentable"
+moderator.register(Blog, BlogModerator)
